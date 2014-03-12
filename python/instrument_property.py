c034ec7812d6b0581f1ab601bccd3f9901d63549
Martin Lichtman <mlichtman@wisc.edu>
2014-03-12 15:57:01 -0500

    Wrote Prop.toHDF5

--- python/instrument_property.py
+++ python/instrument_property.py
@@ -45,7 +45,43 @@ class Prop(Atom):
                 except Exception as e:
                     logger.warning('Evaluating '+p+' in '+self.name+'.properties.\n'+str(e)+str(traceback.format_exc())+'\n')
                     raise PauseError
-
+    
+    def toHDF5(self,hdf_parent_node):
+        '''This function provides generic behavior to save a Prop as an HDF5 group.  The choice of group has been made because a Prop in
+        general can have subproperties, and using a dataset would limit this behavior.
+        We go through the properties list.  If an item has its own toHDF5 method, that will be used.
+        Else, it will be pickled using python's human readable pickling format. And saved to the HDF5 as a string.
+        We assume that settings files will only ever be accessed as read-only and so variable length strings are not bothered with.
+        The node name is self.name, because nodes must have unique names, and so node types are not used.'''
+        
+        #create the group that represents this Prop
+        my_node=hdf_parent_node.create_group(self.name)
+        
+        #go through the list of properties:
+        for p in self.properties:
+            
+            #convert the string name to an actual object
+            try:
+                o=getattr(self,p)
+            except:
+                logger.warning('In Prop.toXML() for class '+self.name+': item '+p+' in properties list does not exist.\n')
+                continue
+            
+            if hasattr(o,'toHDF5'):
+                try:
+                    o.toHDF5(my_node)
+                except:
+                    logger.warning('While trying '+p+'.toHDF5() in Prop.toHDF5() in '+self.name+'.\n'+str(e)+'\n')
+                    raise PauseError
+                
+                        #else just pickle it
+            else:
+                try:
+                    my_node[p]=pickle.dumps(o)
+                except Exception as e:
+                    logger.warning('While picking '+p+' in Prop.toHDF5() in '+self.name+'.\n'+str(e)+'\n')
+                    raise PauseError
+    
     def toXML(self):
         '''This function provides generic XML saving behavior for this package.
         It goes through the properties list. If an item has its own toXML method, that will be used.
@@ -106,7 +142,7 @@ class Prop(Atom):
         try:
             return '<{}>{}</{}>\n'.format(self.name,output,self.name)
         except Exception as e:
-            logger.warning('While in format() in Prop.XMLProtocol() in '+self.name+'.\n'+str(e)+'\n')
+            logger.warning('While in format() in Prop.toHardware() in '+self.name+'.\n'+str(e)+'\n')
             return ''
     
     def HardwareProtocol(self,o,name):
