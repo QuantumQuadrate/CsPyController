e56300025a9ffc6983ce25dd54f727c206467c9c
AQuA_Control Computer <mlichtman@wisc.edu>
2015-02-24 12:11:05 -0600

    GST changes

--- python/gate_set_tomography.py
+++ python/gate_set_tomography.py
@@ -20,16 +20,17 @@ def template_to_HSDIO_scripts(path):
 
     # create an empty list to hold a script string for each sequence
     scripts = []
+    lengths=[]
 
     # for each sequence
     for i, s in enumerate(sequences):
 
         # start with an empty script
         script = ''
+        length=0
 
         # go until we've parsed the whole sequence
         while len(s) > 0:
-
             # check if the remaining sequence starts with a known gate
             # if so, add it to the script
             if s.startswith('{}'):
@@ -37,16 +38,20 @@ def template_to_HSDIO_scripts(path):
             elif s.startswith('Gx'):
                 script += '  compressedGenerate Gx\n'
                 s = s[2:]
+                length += 1
             elif s.startswith('Gy'):
                 script += '  compressedGenerate Gy\n'
                 s = s[2:]
+                length += 1
             elif s.startswith('Gi'):
                 script += '  compressedGenerate Gi\n'
                 s = s[2:]
+                length += 1
             # in the case of parenthesis, parse through the whole statement to be repeated
             elif s.startswith('('):
                 # make a mini script for the repeat
                 repeat_script = ''
+                repeat_length = 0
                 s = s[1:]  # remove the '('
                 # go until the close parenthesis
                 while s[0] != ')':
@@ -56,14 +61,17 @@ def template_to_HSDIO_scripts(path):
                     elif s.startswith('Gx'):
                         repeat_script += '    compressedGenerate Gx\n'
                         s = s[2:]
+                        repeat_length += 1
                     elif s.startswith('Gy'):
                         repeat_script += '    compressedGenerate Gy\n'
                         s = s[2:]
+                        repeat_length += 1
                     elif s.startswith('Gi'):
                         repeat_script += '    compressedGenerate Gi\n'
                         s = s[2:]
+                        repeat_length += 1
                     else:
-                        print "unrecognized input {} in sequence {}\n{}\n".format(s[:1],i,sequences[i])
+                        print "unrecognized input {} in sequence {}\n{}\n".format(s[:1], i, sequences[i])
                         break
                 # the parenthetical statement is now fully parsed
                 s = s[1:]  # remove the ')'
@@ -80,12 +88,15 @@ def template_to_HSDIO_scripts(path):
                         script += '  repeat ' + repeats + '\n'
                         script += repeat_script
                         script += '  end repeat\n'
+                    length += repeat_length*int(repeats)
                 else:
                     script += repeat_script
+                    length += repeat_length
             else:
                 print "unrecognized input {} in sequence {}\n{}\n".format(s[:1],i,sequences[i])
                 break
         # save the script
         scripts += [script]
+        lengths += [length]
     print '{} scripts generated'.format(len(scripts))
-    return scripts
+    return scripts, lengths
