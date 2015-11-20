d1f1e803356472778540227f57c630661cf98cbc
Donald Booth <dbooth2@wisc.edu>
2015-11-20 11:41:31 -0600

    Vaunix code
    Adding Vaunix code and DLLs. Works in test mode; still need to test the
    real device.

--- python/vaunix.py
+++ python/vaunix.py
@@ -21,12 +21,12 @@ from cs_instruments import Instrument
 import serial
 from cs_errors import PauseError
 
-from ctypes import c_int, c_double, byref, c_char_p, CDLL
+from ctypes import *
 import os
 
 class Vaunix(Prop):
     isInitialized = Bool(False)
-    ID = Str()
+    ID = Int()
     va = Member()
     
     frequency = Member()
@@ -38,7 +38,15 @@ class Vaunix(Prop):
     endfreq = Member()
     sweeptime = Member()
     
-    maxPower = Float()
+    sweepmode = Bool()
+    sweeptype = Bool()
+    sweepenable = Bool()
+    sweepdir = Bool()
+    internalref = Bool()
+    useexternalmod = Bool()
+    rfonoff = Bool()
+    
+    maxPower = Int()
     
 
     def __init__(self, name, experiment, description=''):
@@ -47,12 +55,11 @@ class Vaunix(Prop):
         self.power = FloatProp('Power', experiment, 'Power (dBm)', '0')
         self.pulsewidth = FloatProp('PulseWidth', experiment, 'Pulse Width (us)', '0')
         self.pulserep = FloatProp('PulseRep', experiment, 'Pulse Rep Time (us)', '0')
-        #self.pulseenable = BoolProp('PulseEnable', experiment, 'Pulse Enable', '0')
         self.startfreq = FloatProp('StartFreq', experiment, 'Start Frequency (MHz)', '0')
         self.endfreq = FloatProp('EndFreq', experiment, 'End Frequency (MHz)', '0')
-        self.sweeptime = FloatProp('SweepTime', experiment, 'Sweep Time (ms)', '0')
-        self.properties += ['ID', 'Frequency','Power','PulseWidth','PulseRep','PulseEnable','StartFreq','EndFreq','SweepTime'
-                            ]
+        self.sweeptime = IntProp('SweepTime', experiment, 'Sweep Time (ms)', '0')
+        self.properties += ['ID', 'frequency','power','pulsewidth','pulserep','pulseenable','startfreq','endfreq','sweeptime',
+                            'sweepmode', 'sweeptype', 'sweepdir', 'sweepenable', 'internalref', 'useexternalmod', 'rfonoff', 'maxPower']
     
     def initialize(self,va):
         self.va = va
@@ -62,22 +69,34 @@ class Vaunix(Prop):
     def freq_unit(self,val):
         return int(val*10000)
     
+    def power_unit(self,value):
+        return int((self.maxPower - value)/0.25)
         
     def update(self):
-        self.va.fnLMS_SetFrequency(self.ID, freq_unit(self.frequency.value))
-        self.va.fnLMS_SetPowerLevel(self.ID, int((self.maxPower - self.power)/0.25))
-        self.va.fnLMS_SetStartFrequency(self.ID, freq_unit(self.startfreq.value))
-        self.va.fnLMS_SetEndFrequency(self.ID, freq_unit(self.endfreq.value))
+        #print "Setting Frequency to {}x10 Hz".format(self.freq_unit(self.frequency.value))
+        self.va.fnLMS_SetFrequency(self.ID, self.freq_unit(self.frequency.value))
+        #print "Setting Power Level to {} dBm. Max Power {} dBm.".format(self.power.value, self.maxPower)
+        self.va.fnLMS_SetPowerLevel(self.ID, self.power_unit(self.power.value))
+        #print "Setting Start Frequency to {}x10Hz".format(self.freq_unit(self.startfreq.value))
+        self.va.fnLMS_SetStartFrequency(self.ID, self.freq_unit(self.startfreq.value))
+        #print "Setting End Frequency to {}x10Hz".format(self.freq_unit(self.endfreq.value))
+        self.va.fnLMS_SetEndFrequency(self.ID, self.freq_unit(self.endfreq.value))
+        
         self.va.fnLMS_SetSweepTime(self.ID, self.sweeptime.value)
-        self.va.fnLMS_SetUseInternalRef(self.ID, True)  #True: internal ref, False: external ref
+        
+        self.va.fnLMS_SetUseInternalRef(self.ID, self.internalref)  #True: internal ref, False: external ref
+        
         self.va.fnLMS_SetSweepDirection(self.ID, self.sweepdir)
+        
         self.va.fnLMS_SetSweepMode(self.ID, self.sweepmode)  #True: Repeat Sweep, False: Sweep Once
+        
         self.va.fnLMS_SetSweepType(self.ID, self.sweeptype)  #True: Bidirectional Sweep, False: Unidirectional Sweep
-        self.va.fnLMS_SetFastPulsedOutput(self.ID, self.pulsewidth*1e-6, self.pulserep*1e-6, self.pulseenable)
-        
+        #print "Setting FastPulsedOutput"
+        self.va.fnLMS_SetFastPulsedOutput(self.ID, c_float(self.pulsewidth.value*1e-6), c_float(self.pulserep.value*1e-6), self.pulseenable)
+        #print "SetUseExternalPulseMod"
         self.va.fnLMS_SetUseExternalPulseMod(self.ID, self.useexternalmod)
-        
-        self.va.fnLMS_SetRFOn(self.ID, True)
+        #print "SetRFOn"
+        self.va.fnLMS_SetRFOn(self.ID, self.rfonoff)
         self.va.fnLMS_StartSweep(self.ID, self.sweepenable)
         return
         
@@ -88,21 +107,28 @@ class Vaunixs(Instrument):
     motors = Member()
     isInitialized = Bool(False)
     va = Member()
+    
+    testMode = Bool(True)
 	
     def __init__(self, name, experiment, description=''):
         super(Vaunixs, self).__init__(name, experiment, description)
         self.motors = ListProp('motors', experiment, 'A list of individual Vaunix signal generators', listElementType=Vaunix,
                                listElementName='Vaunix')
         self.properties += ['version', 'motors']
+        num = self.initialize()
+        self.motors.length = num
+        self.motors.refreshGUI()
 
     #Initialize: loads and initializes DLL
     def initialize(self):
-        CDLL_file = "VNX_fmsynth.dll"
+        CDLL_file = "vaunix/VNX_fmsynth.dll"
         self.va = CDLL(CDLL_file)
-        self.va.fnLDA_SetTestMode(False)
-        self.detect_generators()
+        if (self.testMode):
+            logger.warning("Warning: Vaunix in test mode. Set testMode=False in vaunix.py to turn off test mode.")
+        self.va.fnLMS_SetTestMode(self.testMode)   #Test mode... this needs to be set False for actual run. Do not remove this command (default setting is True).
         self.isInitialized = True
-        return
+        num = self.detect_generators()
+        return num
 		
     def preExperiment(self, hdf5):
         if self.enable:
@@ -124,8 +150,6 @@ class Vaunixs(Instrument):
             try:
                 for i in self.motors:
                     i.update()
-                    if i.test_output() == False:
-                        i.enable_output()
             except Exception as e:
                 logger.error('Problem updating Vaunix:\n{}\n{}\n'.format(msg, e))
                 self.isInitialized = False
@@ -148,13 +172,15 @@ class Vaunixs(Instrument):
     def detect_generators(self):
         if (not self.isInitialized):
             self.initialize()
-        num=va.fnLMS_GetNumDevices()
-        while (num>self.motors.len):
+        num=self.va.fnLMS_GetNumDevices()
+        logger.debug("Number of vaunix devices detected: {}".format(num))
+        while (num>len(self.motors)):
             self.motors.add()
-        while (num<self.motors.len):
-            self.motors.remove(self.motors.len - 1)
-        devinfo = c_int()*num
-        self.va.fnLMS_GetDevInfo(by_ref(devinfo))
+        while (num<len(self.motors)):
+            self.motors.remove(len(self.motors) - 1)
+        devinfotype = c_uint*num
+        devinfo = devinfotype()
+        self.va.fnLMS_GetDevInfo(addressof(devinfo))
         for mn, i in enumerate(self.motors):
-            i.ID = devinfo[mn]
-        return
+            i.ID = int(devinfo[mn])
+        return num
