584f61833aeade241b8dee17c0cefd0c386e3eff
AQuA_Control Computer <mlichtman@wisc.edu>
2015-12-05 13:47:26 -0600

    working on adding errorbars, fixed a few bugs

--- python/optimization.py
+++ python/optimization.py
@@ -236,7 +236,7 @@ class Optimization(AnalysisWithFigure):
 
         # plot cost with statistical error bars
         ax = fig.add_subplot(self.axes+2, 1, 2)
-        ax.errorbar(range(len(self.ylist)), self.ylist, yerr=self.y_stat_sigma_list)
+        ax.errorbar(range(len(self.ylist)), self.ylist, yerr=self.y_stat_sigma_list, fmt='o')
         ax.set_ylabel('cost with error bar')
 
         # plot settings
