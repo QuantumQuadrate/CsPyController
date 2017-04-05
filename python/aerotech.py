41da2ba4ed05f52b24bab2d23829e87feee91fb5
Jambo <hostetter@wisc.edu>
2017-04-05 12:00:15 -0500

    Adding missing files (always remember to run git add -A)

---
+++ python/aerotech.py
@@ -0,0 +1,127 @@
+from __future__ import division
+
+"""
+aerotech.py
+
+part of the CsPyController package for AQuA experiment control by Martin Lichtman
+
+Handles sending global variable updates to the Aerotech Ensemble translation stage.
+This python code sends commands via TCP to a server running in C#, also in this package.
+The C# server then uses the .NET assembly provided by Aerotech to talk to the Ensemble driver.
+
+created = 2015.06.22
+modified >= 2015.06.22
+"""
+
+__author__ = 'Martin Lichtman'
+import logging
+logger = logging.getLogger(__name__)
+
+from atom.api import Bool, Str, Member, Int
+from instrument_property import Prop, FloatProp, IntProp, ListProp
+from cs_instruments import Instrument
+import TCP
+from cs_errors import PauseError
+import subprocess
+
+class Aerotech(Prop):
+    Xi = Member()
+    Xend = Member()
+    Xvmx = Member()
+    Xamx = Member()
+    Zi = Member()
+    Zend = Member()
+    Zvmx = Member()
+    Zamx = Member()
+    XretTrig = Member()
+
+    def __init__(self, name, experiment, description=''):
+        super(Aerotech, self).__init__(name, experiment, description)
+        self.Xi = FloatProp('Xi', experiment, 'Initial X position (mm)','0')
+        self.Xend = FloatProp('Xend', experiment, 'Final X position (mm)','0')
+        self.Xvmx = FloatProp('Xvmx', experiment, 'Max X velocity (mm/s)','0')
+        self.Xamx = FloatProp('Xend', experiment, 'Max X acceleration (mm/s^2)','0')
+        self.Zi = FloatProp('Zi', experiment, 'Initial Z position (mm)','0')
+        self.Zend = FloatProp('Zend', experiment, 'Final Z position (mm)','0')
+        self.Zvmx = FloatProp('Zvmx', experiment, 'Max Z velocity (mm/s)','0')
+        self.Zamx = FloatProp('Zend', experiment, 'Max Z acceleration (mm/s^2)','0')
+        self.XretTrig = IntProp('XretTrig', experiment, 'X Trig Return?','0')
+        self.properties += ['Xi', 'Xend', 'Xvmx', 'Xamx', 'Zi', 'Zend', 'Zvmx', 'Zamx', 'XretTrig']
+
+    def update(self):
+        return 'UpdateGlobals,{},{},{},{},{},{},{},{},{}'.format(self.Xi.value, self.Xend.value, self.Xvmx.value,self.Xamx.value ,self.Zi.value, self.Zend.value, self.Zvmx.value, self.Zamx.value, self.XretTrig.value)
+
+
+class Aerotechs(Instrument):
+    version = '2015.06.22'
+    IP = Str()
+    port = Int()
+    motors = Member()
+    socket = Member()
+    OneAerotech=Member()
+
+    def __init__(self, name, experiment, description=''):
+        super(Aerotechs, self).__init__(name, experiment, description)
+        self.motors = ListProp('motors', experiment, 'A list of individual Aerotech stages', listElementType=Aerotech,
+                               listElementName='motor')
+        self.OneAerotech = Aerotech('OneAerotech',experiment,'One Aerotech Ensemble')
+        self.properties += ['version', 'IP', 'port', 'motors','OneAerotech']
+
+    def launchServer(self):
+        subprocess.Popen(["C:\\Windows\\System32\\cmd.exe","/C","..\\csharp\\Aerotech_Ensemble_Server\\bin\\Debug\\Ensemble Console Example CSharp.exe"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+
+    def preExperiment(self, hdf5):
+        """Open the TCP socket"""
+        if self.enable:
+            self.socket = TCP.CsClientSock(self.IP, self.port)
+            logger.debug("Aerotech: preExperiment: sending WaitForGlobals")
+            self.socket.sendmsg("WaitForGlobals")
+            # TODO: add here some sort of communications check to see if it worked
+
+            self.isInitialized = True
+
+    def postMeasurement(self, measurementresults, iterationresults, hdf5):
+        return
+
+    def postIteration(self, iterationresults, hdf5):
+        if self.enable:
+            self.socket.sendmsg("WaitForGlobals")
+        return
+
+    def postExperiment(self, hdf5):
+        return
+
+    def finalize(self,hdf5):
+        return
+
+    def preIteration(self, iterationresults, hdf5):
+        """
+        Every iteration, send the motors updated positions.
+        """
+        if (not self.isInitialized):
+            self.preExperiment(hdf5)
+        if self.enable:
+            msg = ''
+            try:
+                #for i in self.motors:
+                #    msg = i.update()
+                    # send update to the aerotech server
+                #    self.socket.sendmsg(msg)
+                msg = self.OneAerotech.update()
+                self.socket.sendmsg(msg)
+            except Exception as e:
+                logger.error('Problem setting Aerotech globals, closing socket:\n{}\n{}\n'.format(msg, e))
+                self.socket.close()
+                self.isInitialized = False
+                raise PauseError
+
+    def parUpdate(self):
+        """
+        Passes WaitForGlobals value update and then runs the preiteration step
+        :return:
+        """
+        self.preIteration(0,0)
+        self.socket.sendmsg("WaitForGlobals")
+        self.preIteration(0,0)
+        return
---
+++ python/conex.py
@@ -0,0 +1,144 @@
+from __future__ import division
+
+"""
+conex.py
+
+part of the CsPyController package for AQuA experiment control by Martin Lichtman
+
+Handles sending global variable updates to the Newport CONEX-CC translation stage.
+This python code sends commands via TCP to a server running in C#, also in this package.
+The C# server then uses the .NET assembly provided by Newport to talk to the CONEX-CC driver.
+
+created = 2015.06.29
+modified >= 2015.06.29
+"""
+
+__author__ = 'Martin Lichtman'
+import logging
+logger = logging.getLogger(__name__)
+
+from atom.api import Bool, Str, Member, Int
+from instrument_property import Prop, FloatProp, IntProp, ListProp, BoolProp, StrProp
+from cs_instruments import Instrument
+import TCP
+from cs_errors import PauseError
+import time
+import subprocess
+
+class Conex(Prop):
+    SetPos = Member()
+    Vel = Member()
+    enableVel = Bool()
+    IDString = Member()
+    Threshold = Member()
+
+    def __init__(self, name, experiment, description=''):
+        super(Conex, self).__init__(name, experiment, description)
+        self.IDString = StrProp('IDString', experiment, 'Instrument Key','0')
+        self.SetPos = FloatProp('SetPos', experiment, 'Set position (mm)','0')
+        self.Vel = FloatProp('Vel', experiment, 'Velocity (mm/s)','0')
+        self.Threshold = FloatProp('PositionThreshold', experiment, 'Threshold for Position (mm)','0')
+        self.properties += ['SetPos', 'enableVel', 'Vel', "IDString",'Threshold']
+
+        
+    def initialize(self):
+        logger.debug('CONEX IDString: {}'.format(self.IDString.value))
+        return 'Init,{}'.format(self.IDString.value)
+    
+    def update(self):
+        if (self.enableVel == False):
+            logger.warning('CONEX position: {}'.format(self.SetPos.value))
+            return 'SetPosition,{}'.format(self.SetPos.value)
+        else:
+            return 'SetPositionVelocity,{},{}'.format(self.SetPos.value,self.Vel.value)
+
+
+class Conexes(Instrument):
+    version = '2015.06.22'
+    IP = Str()
+    port = Int()
+    motors = Member()
+    socket = Member()
+
+    def __init__(self, name, experiment, description=''):
+        super(Conexes, self).__init__(name, experiment, description)
+        self.motors = ListProp('motors', experiment, 'A list of individual CONEX-CC stages', listElementType=Conex,
+                               listElementName='motor')
+        self.properties += ['version', 'IP', 'port', 'motors']
+
+    def launchServer(self):
+        subprocess.Popen(["C:\\Windows\\System32\\cmd.exe","/C","..\\csharp\\CONEX_Server\\bin\\Debug\\CONEX-CC-CSharp-Server.exe"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+    def preExperiment(self, hdf5):
+        """Open the TCP socket"""
+        if self.enable:
+            self.socket = TCP.CsClientSock(self.IP, self.port)
+            try:
+                for i in self.motors:
+                    msg = i.initialize()
+                    self.socket.sendmsg(msg)
+                    logger.debug("CONEX: Sent initialization message, waiting for response")
+                    returnedmessage = self.socket.receive()
+                    logger.debug("CONEX: Received response: {}".format(returnedmessage))
+                    if (returnedmessage != "Success"):
+                        logger.error('Problem initializing Conex: \n{}\n'.format(returnedmessage))
+                        self.isInitialized = False
+                        return PauseError
+            except Exception as e:
+                logger.error('Problem initializing Conex: \n{}\n{}\n'.format(msg,e))
+                self.isInitialized = False
+                return PauseError
+            # TODO: add here some sort of communications check to see if it worked
+
+            self.isInitialized = True
+
+
+    def postMeasurement(self, measurementresults, iterationresults, hdf5):
+        return
+
+    def postIteration(self, iterationresults, hdf5):
+        return
+
+    def postExperiment(self, hdf5):
+        return
+
+    def finalize(self, hdf5):
+        if self.enable:
+            self.socket.close()
+        return
+
+    def preIteration(self, iterationresults, hdf5):
+        """
+        Every iteration, send the motors updated positions.
+        """
+        if self.enable:
+            msg = ''
+            try:
+                for i in self.motors:
+                    msg = i.update()
+                    # send update to the conex server
+                    logger.debug("Conex: About to send update to CONEX server")
+                    self.socket.sendmsg(msg)
+                    logger.debug("Conex: Sent update to CONEX server. About to ask position")
+                    self.socket.sendmsg("GetPosition")
+                    logger.debug("Conex: Asked position. Waiting for response")
+                    returnedmessage = self.socket.receive()
+                    logger.debug("Conex: Received response: {}".format(returnedmessage))
+                    curPos = float(returnedmessage)
+                    loopCount=0
+                    loopThreshold=100
+                    while(abs(i.SetPos.value - curPos) > i.Threshold.value and loopCount < loopThreshold):  #loop until the error is less than threshold
+                        time.sleep(0.1)
+                        self.socket.sendmsg("GetPosition")
+                        returnedmessage = self.socket.receive()
+                        curPos = float(returnedmessage)
+                        loopCount = loopCount+1
+                    if (loopCount>=loopThreshold):
+                        logger.warning("Conex: Did not converge to correct position in 10 seconds. Is the threshold too small?")
+                    else:
+                        logger.debug("Conex: Converged to position.")
+            except Exception as e:
+                logger.error('Problem setting Conex positions, closing socket:\n{}\n{}\n'.format(msg, e))
+                self.socket.close()
+                self.isInitialized = False
+                raise PauseError
