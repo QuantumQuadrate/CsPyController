e2149e74a9190dd634320a16ebb9ff47ea61df92
Minho Kwon <kwnminho@gmail.com>
2017-05-17 12:59:49 -0500

    functional waveform : unused dds pins can be skipped when setting HSDIO channels

--- python/exp_functional_waveforms/dds.py
+++ python/exp_functional_waveforms/dds.py
@@ -8,13 +8,13 @@ MOT = DDS(
     HSDIO,                                      # pass in HSDIO function so it can be used in the class
     (32, 33, 34),                               # HSDIO channels for profile pins
     {                                           # dict with channel descriptions and pin settings
-        'MOT':(0,0,0), 
-        'PGC in MOT':(1,0,0), 
-        'off':(0,1,0), 
-        'monitor':(0,0,1), 
-        'readout':(1,1,0), 
-        'light assisted collisions':(1,0,1), 
-        'Blowaway':(0,1,1), 
+        'MOT':(0,0,0),
+        'PGC in MOT':(1,0,0),
+        'off':(0,1,0),
+        'monitor':(0,0,1),
+        'readout':(1,1,0),
+        'light assisted collisions':(1,0,1),
+        'Blowaway':(0,1,1),
         'PGC in traps':(1,1,1)}
     ).profile
 
@@ -29,61 +29,62 @@ class DDS(object):
     This class does NOT communicate directly with the DDS box, that is taken care of in the DDS.py file.
     This class only manipulates the HSDIO channels which switch the current HSDIO profile.
     It takes care of grey coding the profile changes.
-    This class only works properly if all calls to a particular instance are done sequentially, 
+    This class only works properly if all calls to a particular instance are done sequentially,
     but that is the expected situation for a single DDS channel.
     In other words, the sequencing for a DDS() object must be done monotonically."""
-    
+
     def __init__(self, HSDIO, channels, profiles=None, t=0):
         """Create a new DDS channel.
         channels:   a list of the HSDIO channels corresponding to the DDS bits.  e.g. (0, 1, 18)
-        profile:    a dict using profile names as keys to look up the bit settings.  
+        profile:    a dict using profile names as keys to look up the bit settings.
                     e.g. {"MOT": (0,0,0), "OFF": (1,0,0)}
         """
         self.HSDIO = HSDIO
         self.channels = channels
         self.profiles = profiles
-        
+
         # keep track of the state
         self.bits = [False, False, False]
         # keep track of the last time a bit was changed, to see if we need to add a DDS delay
         # assume that a bit was changed immediately before this, to be conservative
         self.last_change = t
-    
+
     def initialize(self, t, new_bits):
         """Sets the state.  Unlike set(), this function sets every bit, regardless of its current state."""
         # set the state in the HSDIO, adding a delay between each bit setting if necessary
         for channel, new_bit in zip(self.channels, new_bits):
-            t = self.delay(t)
-            self.HSDIO(t, channel, new_bit)
+            if(channel!=-1):
+                t = self.delay(t)
+                self.HSDIO(t, channel, new_bit)
         # keep track that the bits have all been set
         self.bits = new_bits
         return t
-    
+
     def delay(self, t):
         """Add a DDS delay if it is needed, and update the last_change parameter."""
-
+        DDS_profile_delay=0 # DDS delay set to 0. It needs to be accessible externally.
         if t <= (self.last_change + DDS_profile_delay):
             t += DDS_profile_delay
         # update the last_change parameter
         self.last_change = t
         return t
-    
+
     def set(self, t, new_bits):
         """Sets the state.  Only bits that need to be flipped are flipped.  Grey coding is automatic.
         new_bits: the state we want to set"""
 
         # go through the bits one at a time
         for channel, old_bit, new_bit in zip(self.channels, self.bits, new_bits):
-            # check to see if the bit needs to be changed
-            if old_bit != new_bit:
-                # delay if we recently changed another bit
-                t = self.delay(t)
-                self.HSDIO(t, channel, new_bit)
+            if(channel!=-1):
+                # check to see if the bit needs to be changed
+                if old_bit != new_bit:
+                    # delay if we recently changed another bit
+                    t = self.delay(t)
+                    self.HSDIO(t, channel, new_bit)
         # keep track that the bits have all been set
         self.bits = new_bits
         return t
-     
+
     def profile(self, t, profile):
         """Switch to a specific profile by looking up the name in the stored dict."""
         return self.set(t, self.profiles[profile])
-
--- python/exp_functional_waveforms/functional_waveforms_rb.py
+++ python/exp_functional_waveforms/functional_waveforms_rb.py
@@ -41,52 +41,76 @@ overwritten at the global scope.
 '''
 
 ################################################################################
-# MOT DDS SETUP ################################################################
+# 3D MOT DDS SETUP ################################################################
 ################################################################################
-mot_dds_pinout = (1,2,3)
-mot_dds_profiles = {
+mot_3d_dds_pinout = (0,1,2)
+mot_3d_dds_profiles = {
     'MOT' : (0,0,0),
-    'PGC' : (1,0,0),
-    'OFF' : (0,1,0),
-    'MON' : (0,0,1),
-    'RO'  : (1,1,0),
-    'BA'  : (1,0,1),
-    'COOL': (0,1,1),
-    'P7'  : (1,1,1)
+    'off' : (0,0,1),
+    'Blowaway' : (0,1,0),
+    'RO' : (0,1,1)
 }
+
+################################################################################
+# FORT DDS SETUP ################################################################
+################################################################################
+fort_dds_pinout = (-1,3,4) # -1 indicates pins that are not being used
+fort_dds_profiles = {
+    'on' : (0,0,0),
+    'off' : (0,0,1),
+    'Blowaway' : (0,1,0),
+    'RO' : (0,1,1)
+}
+################################################################################
+# 2D MOT DDS SETUP ################################################################
+################################################################################
+mot_2d_dds_pinout = (-1,-1,5) # -1 indicates pins that are not being used
+mot_2d_dds_profiles = {
+    'on' : (0,0,0),
+    'off' : (0,0,1)
+}
+################################################################################
+# OP DDS SETUP ################################################################
+################################################################################
+op_dds_pinout = (-1,-1,6) # -1 indicates pins that are not being used
+op_dds_profiles = {
+    'on' : (0,0,0),
+    'off' : (0,0,1)
+}
+
 ################################################################################
 # MOT AOM SWITCH SETUP #########################################################
 ################################################################################
-mot_aom_switch_chan = 4
+mot_aom_switch_chan = 7
 # this is the default profile, we dont have to pass it in if we dont want to
-mot_aom_switch_profile = {'on':1, 'off':0} 
+mot_aom_switch_profile = {'on':1, 'off':0}
 # timing delay parameter
 mot_aom_switch_delay = 0
 
 ################################################################################
 # HF AOM SWITCH SETUP ##########################################################
 ################################################################################
-hf_aom_switch_chan = 5
+hf_aom_switch_chan = 11
 # this is the default profile, we dont have to pass it in if we dont want to
-hf_aom_switch_profile = {'on':1, 'off':0} 
+hf_aom_switch_profile = {'on':1, 'off':0}
 # timing delay parameter
 hf_aom_switch_delay = 0
 
 ################################################################################
 # FORT AOM SWITCH SETUP ########################################################
 ################################################################################
-fort_aom_switch_chan = 6
+fort_aom_switch_chan = 8
 # this is the default profile, we dont have to pass it in if we dont want to
-fort_aom_switch_profile = {'on':1, 'off':0} 
+fort_aom_switch_profile = {'on':1, 'off':0}
 # timing delay parameter
 fort_aom_switch_delay = 0
 
 ################################################################################
 # ANDOR EMCCD CAMERA SETUP #####################################################
 ################################################################################
-andor_trigger_chan = 7
+andor_trigger_chan = 20
 # this is the default profile, we dont have to pass it in if we dont want to
-andor_trigger_profile = {'on':1, 'off':0} 
+andor_trigger_profile = {'on':1, 'off':0}
 # timing delay parameter
 andor_trigger_delay = 0
 
@@ -99,46 +123,48 @@ class Rb(object):
     '''
 
     def __init__(self, HSDIO, AO, DO, label):
-        self.mot_dds = DDS(HSDIO, mot_dds_pinout, mot_dds_profiles)
+        self.mot_3d_dds = DDS(HSDIO, mot_3d_dds_pinout, mot_3d_dds_profiles)
+        self.fort_dds = DDS(HSDIO, fort_dds_pinout, fort_dds_profiles)
+        self.mot_2d_dds = DDS(HSDIO, mot_2d_dds_pinout, mot_2d_dds_profiles)
+        self.op_dds = DDS(HSDIO, op_dds_pinout, op_dds_profiles)
 
         self.mot_aom_switch = Switch(
-            HSDIO, 
-            mot_aom_switch_chan, 
-            profiles=mot_aom_switch_profile, 
+            HSDIO,
+            mot_aom_switch_chan,
+            profiles=mot_aom_switch_profile,
             delay=mot_aom_switch_delay
         )
 
         self.hf_aom_switch = Switch(
-            HSDIO, 
-            hf_aom_switch_chan, 
-            profiles=hf_aom_switch_profile, 
+            HSDIO,
+            hf_aom_switch_chan,
+            profiles=hf_aom_switch_profile,
             delay=hf_aom_switch_delay
         )
 
         self.fort_aom_switch = Switch(
-            HSDIO, 
-            fort_aom_switch_chan, 
-            profiles=fort_aom_switch_profile, 
+            HSDIO,
+            fort_aom_switch_chan,
+            profiles=fort_aom_switch_profile,
             delay=fort_aom_switch_delay
         )
 
         self.camera = Camera(
-            HSDIO, 
+            HSDIO,
             andor_trigger_chan
         )
 
 #    def mot_dds(self):
-#        return 
+#        return
 #
 #    def mot_aom_switch(self, t, profile):
 #        return self.mot_aom_switch.profile(t, profile)
 #
-#    def hf_aom_switch(self, t, profile): 
+#    def hf_aom_switch(self, t, profile):
 #        return self.hf_aom_switch.profile(t, profile)
 #
-#    def fort_aom_switch(self): 
+#    def fort_aom_switch(self):
 #        return self.fort_aom_switch.profile(t, profile)
 #
-#    def andor_take_shot(self): 
+#    def andor_take_shot(self):
 #        return self.camera.take_shot(t)
-
