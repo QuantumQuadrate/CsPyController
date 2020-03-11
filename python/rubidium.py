3ebc4c85616d2f9d5d661be114d77cc1568da318
Christopher Young <cbyoung@wisc.edu>
2020-03-11 12:16:23 -0500

    fix rb.py

--- python/rubidium.py
+++ python/rubidium.py
@@ -5,32 +5,27 @@ from cs_errors import PauseError
 from atom.api import Member, Int
 
 # Bring in other files in this package
-from ConfigInstrument import Config
-import functional_waveforms, analysis, instek_pst, save2013style, TTL, LabView
-import BILT
-import noise_eaters
-import DDS, roi_fitting
-import picomotors, andor, picampython, vaunix, DCNoiseEater, Laird_temperature, AnalogInput
-import Counter, unlock_pause, niscope, newportstage, nidaq_ai
+import functional_waveforms, analysis, save2013style, TTL, LabView
+import DDS
+import andor, AnalogInput
+import unlock_pause, nidaq_ai
 logger = logging.getLogger(__name__)
 import origin_interface
-import FakeInstrument  # for testing
 from pypico import PyPicoServer  # for communicating with a picomotor server
 
 # analyses
 from SquareROIAnalysis import SquareROIAnalysis
 from RbAIAnalysis import RbAIAnalysis
-# from recent_shot_analysis import RecentShotAnalysis
-# from image_sum_analysis import ImageSumAnalysis
+from recent_shot_analysis import RecentShotAnalysis
+from image_sum_analysis import ImageSumAnalysis
 from threshold_analysis import ThresholdROIAnalysis
 from retention_analysis import RetentionAnalysis, RetentionGraph
-from histogram_analysis import HistogramAnalysis, HistogramGrid
+from histogram_analysis import HistogramAnalysis
+from beam_position_analysis import BeamPositionAnalysis
+
 # from beam_position_analysis import BeamPositionAnalysis
 
 from experiments import Experiment
-
-__author__ = 'Martin Lichtman'
-
 
 
 class Rb(Experiment):
@@ -43,8 +38,14 @@ class Rb(Experiment):
     DDS = Member()
     DDS2 = Member()
     pyPicoServer = Member()
-
+    imageSumAnalysis = Member()
     functional_waveforms = Member()
+    functional_waveforms_graph = Member()
+    DAQmxAI = Member()
+    beam_position_analysis = Member()
+    beam_position_analysis2 = Member()
+    TTL_filters = Member()
+    unlock_pause = Member()
     AI_graph = Member()
     AI_filter = Member()
     squareROIAnalysis = Member()
@@ -61,8 +62,11 @@ class Rb(Experiment):
     iterations_graph = Member()
     retention_graph = Member()
     retention_analysis = Member()
+    recent_shot_analysis = Member()
     save_notes = Member()
+    TTL_filters = Member()
     origin = Member()
+    Ramsey = Member()
     ROI_rows = Int(1)
     ROI_columns = Int(1)
     ROI_bg_rows = Int(0)
@@ -75,9 +79,9 @@ class Rb(Experiment):
                  temp_location=None):
 
         super(Rb, self).__init__(config_instrument=config_instrument,
-                                   cache_location=cache_location,
-                                   settings_location=settings_location,
-                                   temp_location=temp_location)
+                                 cache_location=cache_location,
+                                 settings_location=settings_location,
+                                 temp_location=temp_location)
         try:
             # communicates with Blackfly camera server
             from blackfly import BlackflyClient
@@ -91,19 +95,26 @@ class Rb(Experiment):
 
         self.Andors = andor.Andors('Andors', self, 'Andor Luca measurementResults')
         self.LabView = LabView.LabView(self)
+        self.DAQmxAI = nidaq_ai.NIDAQmxAI('DAQmxAI', self, 'NI-DAQmx Analog Input')
         self.DDS = DDS.DDS('DDS', self, 'server for homemade DDS boxes')
         self.DDS2 = DDS.DDS('DDS2', self, 'XML server for homemade DDS boxes')
         self.pyPicoServer = PyPicoServer('PyPicomotor', self, 'PyPico server interface for controlling closed loop picomotors')
-        self.instruments += [ self.pyPicoServer,
+        self.unlock_pause = unlock_pause.UnlockMonitor('unlock_pause', self, 'Monitor for pausing when laser unlocks')
+        self.instruments += [
+            self.DAQmxAI,
+            self.pyPicoServer,
             self.Andors,
             self.DDS,
-            self.DDS2
+            self.DDS2,
+            self.unlock_pause
         ]
         # Labview must be last at least until someone fixes the start command
         self.instruments += [self.LabView]
 
         # analyses
-        # self.TTL_filters = TTL.TTL_filters('TTL_filters', self)
+        self.imageSumAnalysis = ImageSumAnalysis(self)
+        self.TTL_filters = TTL.TTL_filters('TTL_filters', self)
+        self.functional_waveforms_graph = functional_waveforms.FunctionalWaveformGraph('functional_waveforms_graph', self, 'Graph the HSDIO, DAQmx DO, and DAQmx AO settings')
         self.AI_graph = AnalogInput.AI_Graph('AI_graph', self, 'Analog Input Graph')
         self.AI_filter = AnalogInput.AI_Filter('AI_filter', self, 'Analog Input filter')
         self.first_measurements_filter = analysis.DropFirstMeasurementsFilter('first_measurements_filter', self, 'drop the first N measurements')
@@ -115,13 +126,18 @@ class Rb(Experiment):
         self.text_analysis = analysis.TextAnalysis('text_analysis', self, 'text results from the measurement')
         #self.recent_shot_analysis = RecentShotAnalysis('recent_shot_analysis', self, description='just show the most recent shot')
         self.histogramAnalysis = HistogramAnalysis('histogramAnalysis', self, 'plot the histogram of any shot and roi')
+        self.Ramsey = analysis.Ramsey('Ramsey', self, 'Fit a cosine to retention results')
         self.measurements_graph = analysis.MeasurementsGraph('measurements_graph', self, 'plot the ROI sum vs all measurements')
         self.iterations_graph = analysis.IterationsGraph('iterations_graph', self, 'plot the average of ROI sums vs iterations')
         self.retention_graph = RetentionGraph('retention_graph', self, 'plot occurence of binary result (i.e. whether or not atoms are there in the 2nd shot)')
-        # self.andor_viewer = andor.AndorViewer('andor_viewer', self, 'show the most recent Andor image')
-        # self.picam_viewer = picam.PICamViewer('picam_viewer', self, 'show the most recent PICam image')
         self.retention_analysis = RetentionAnalysis('retention_analysis', self, 'calculate the loading and retention')
         self.save_notes = save2013style.SaveNotes('save_notes', self, 'save a separate notes.txt')
+        self.beam_position_analysis = BeamPositionAnalysis(self)
+        self.beam_position_analysis2 = BeamPositionAnalysis(self)
+        # setup path for second beam position analysis
+        self.beam_position_analysis2.set_position_paths(datagroup='Camera1DataGroup')
+        self.recent_shot_analysis = RecentShotAnalysis('recent_shot_analysis', self, description='just show the most recent shot')
+
         # setup path for second beam position analysis
         # self.vitalsignsound=Vitalsign('vital_sign_sound',self,'beeps when atoms are loaded')
         self.origin = origin_interface.Origin('origin', self, 'saves selected data to the origin data server')
@@ -134,6 +150,7 @@ class Rb(Experiment):
             # ROI analyses go here ------------------------------------------
             self.squareROIAnalysis,
             # ---------------------------------------------------------------
+            self.imageSumAnalysis,
             self.histogramAnalysis, self.thresholdROIAnalysis,
             self.loading_filters, self.error_filters,
             self.first_measurements_filter, self.text_analysis,
@@ -143,10 +160,19 @@ class Rb(Experiment):
             self.Andors,
             self.retention_analysis, self.retention_graph,
             self.save_notes,
-            self.origin  # origin has to be last
+            self.TTL_filters,
+            self.functional_waveforms_graph,
+            self.Ramsey,
+            self.beam_position_analysis,
+            self.beam_position_analysis2,
+            self.recent_shot_analysis,
+            self.origin  # TODO: origin has to be last
         ]
 
         self.properties += [
+            'Ramsey', 'TTL_filters', 'beam_position_analysis',
+            'beam_position_analysis2', 'functional_waveforms_graph',
+            'unlock_pause', 'DAQmxAI', 'imageSumAnalysis'
             'Config', 'RbAIAnalysis', 'functional_waveforms', 'LabView',
             'DDS', 'DDS2',
             'pyPicoServer', 'Andors',
@@ -158,6 +184,7 @@ class Rb(Experiment):
             'retention_analysis', 'measurements_graph', 'iterations_graph',
             'retention_graph',
             'ROI_rows', 'ROI_columns', 'ROI_bg_rows', 'ROI_bg_columns',
+            'recent_shot_analysis',
             'origin'
         ]
 
@@ -167,6 +194,8 @@ class Rb(Experiment):
             'Independent Variables': 'IndependentVariables(independentVariables = main.experiment.independentVariables, creator=main, name="Independent Variables")',
             'Constants and Dependent Vars': 'Variables(experiment = main.experiment, creator=main, name="Constants and Dependent Vars")',
             'PXI Communication': 'LabViewPage(LabView = main.experiment.LabView, creator=main, name="PXI Communication")',
+            'DAQmx': 'DAQmxDigitalOutPage(DAQmx = main.experiment.LabView.DAQmxDO, creator=main, name="DAQmx")',
+            'DAQmxAI': 'DAQmxAI(NIDAQmxAI = main.experiment.DAQmxAI, creator=main, name="DAQmxAI")',
             'HSDIO': 'HSDIO_DigitalOutPage(HSDIO = main.experiment.LabView.HSDIO, creator=main, name="HSDIO")',
             'DDS': 'DDS_Page(DDS = main.experiment.DDS, creator=main, name="DDS")',
             'DDS2':'DDS_Page(DDS = main.experiment.DDS2, creator=main, name="DDS2")',
@@ -174,7 +203,7 @@ class Rb(Experiment):
             'Blackfly Client': 'BlackflyClient(blackfly_client = main.experiment.blackfly_client, creator=main, name="Blackfly Client")',
             'Analog Output': 'AnalogOutput(AO = main.experiment.LabView.AnalogOutput, creator=main, name="Analog Output")',
             'Analog Input': 'AnalogInput(AI = main.experiment.LabView.AnalogInput, filters = main.experiment.AI_filter, analysis = main.experiment.AI_graph, creator=main, name="Analog Input")',
-            'PyPico': 'PyPico(pypico = main.experiment.pyPicoServer, parent=main, name="PyPico")',
+            'PyPico': 'PyPico(pypico = main.experiment.pyPicoServer, creator=main, name="PyPico")',
             'AAS': 'AAS(aas = main.experiment.beam_position_analysis, creator=main, name="AAS")',
             'AAS2': 'AAS(aas = main.experiment.beam_position_analysis2, creator=main, name="AAS2")',
             'Live Images': 'MultiImage(experiment = main.experiment, analysis0 = main.experiment.recent_shot_analysis, analysis2 = main.experiment.imageSumAnalysis, analysis3 = main.experiment.text_analysis, creator=main, name="Live Images")',
@@ -185,7 +214,9 @@ class Rb(Experiment):
             'Iterations Graph': 'IterationsGraph(experiment = main.experiment, analysis = main.experiment.iterations_graph, creator=main, name="Iterations Graph")',
             'Retention Graph': 'RetentionGraph(experiment = main.experiment, analysis = main.experiment.retention_graph, creator=main, name="Retention Graph")',
             'Filters': 'Filters(experiment = main.experiment, creator=main, name="Filters")',
+            'Functional Waveforms Graph': 'FunctionalWaveformsGraph(graph = main.experiment.functional_waveforms_graph, creator=main, name="Functional Waveforms Graph")',
             'Optimization': 'Optimizer(experiment = main.experiment, analysis = main.experiment.optimizer, creator=main, name="Optimization")',
+            'Pi Lock Monitor': 'Unlock_Pause(unlock_pause = main.experiment.unlock_pause, creator=main, name="Pi Lock Monitor")',
             'Retention Analysis': 'RetentionAnalysis(analysis = main.experiment.retention_analysis, creator=main, name="Retention Analysis")',
             'Functional Waveforms': 'FunctionalWaveforms(waveforms = main.experiment.functional_waveforms, creator=main, name="Functional Waveforms")',
             'Origin Interface': 'Origin(origin = main.experiment.origin, creator=main, name="Origin Interface")',
@@ -208,6 +239,5 @@ class Rb(Experiment):
         self.allow_evaluation = True
 
     def exiting(self):
-        self.PICams.__del__()
         self.Andors.__del__()
         return
